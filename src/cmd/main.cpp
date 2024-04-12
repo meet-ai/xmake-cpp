294ba3fb88fd218571d4ddd311203fd220bdcdaa
meetai <meetai@gmx.com>
2024-04-13 00:48:17 +0800

    add matchit,proxy

--- src/cmd/main.cpp
+++ src/cmd/main.cpp
@@ -1,59 +1,8 @@
-
-#include "message.grpc.pb.h"
-#include "message.pb.h"
-
-#include <grpc/grpc.h>
-#include <grpcpp/security/server_credentials.h>
-#include <grpcpp/server.h>
-#include <grpcpp/server_builder.h>
-#include <grpcpp/server_context.h>
 #include <iostream>
 #include <memory>
 #include <string>
+#include <matchit.h>
+#include <proxy.h>
 
-using grpc::Server;
-using grpc::ServerBuilder;
-using grpc::ServerContext;
-using grpc::ServerReader;
-using grpc::ServerReaderWriter;
-using grpc::ServerWriter;
-using grpc::Status;
-using std::cout;
-using std::endl;
-using std::string;
-
-using namespace helloworld;
-class GreeterServiceImpl final : public helloworld::Greeter::Service {
-  Status SayHello(ServerContext *context, const HelloRequest *request,
-                  HelloReply *reply) override {
-    std::string prefix("Hi!");
-    reply->set_message("hi");
-    return Status::OK;
-  }
-};
-
-void RunServer() {
-  std::string server_addr("0.0.0.0:50051");
-  // create an instance of our service implementation class Test1Impl
-  GreeterServiceImpl service;
-
-  // Create an instance of factory ServerBuilder class
-  ServerBuilder builder;
-
-  // Specify the address and port we want to use to listen for client requests
-  // using the builder’s AddListeningPort() method.
-  builder.AddListeningPort(server_addr, grpc::InsecureServerCredentials());
-
-  // Register our service implementation with the builder.
-  builder.RegisterService(&service);
-
-  // Call BuildAndStart() on the builder to create and start an RPC server for
-  // our service.
-  std::unique_ptr<Server> server(builder.BuildAndStart());
-  cout << "Server listening on " << server_addr << endl;
-
-  // Call Wait() on the server to do a blocking wait until process is killed or
-  // Shutdown() is called
-  server->Wait();
+int main() {
 }
-int main() { RunServer(); }
--- xmake.lua
+++ xmake.lua
@@ -15,25 +15,47 @@ add_rules("mode.debug", "mode.release")
 --        --assert(package:has_cfuncs("add", {includes = "foo.h"}))
 --    end)
 --package_end()
+--package("matchit")
+--    add_deps("cmake")
+--    set_sourcedir(path.join(os.scriptdir(), "3rdparty/matchit"))
+--    on_install(function (package)
+--        local configs = {}
+--        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+--        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+--        import("package.tools.cmake").install(package, configs)
+--    end)
+--
+--    on_test(function (package)
+--        --assert(package:has_cfuncs("add", {includes = "foo.h"}))
+--    end)
+--
+--    on_fetch(function (package)
+--          local result = {}
+--          result.links = "matchit"
+--          result.includedirs = package:installdir("include")
+--          result.linkdirs = package:installdir("lib")
+--          return result
+--      end)
+--package_end()
 
 --------------------------------------------------------
-add_cxxflags("-std=c++17")
+add_cxxflags("-std=c++20")
 
 add_requires("zlib")
 add_requires("expat",{system=false})
 add_requires("civetweb",{system=false})
-add_requires("poco")
+--add_requires("poco")
 add_requires("fmt")
 add_requires("spdlog")
-add_requires("apr")
-add_includedirs("/usr/include", "/usr/local/include")
+--add_requires("apr")
+add_requires("conan::matchit/1.0.1", {alias = "matchit", configs = {}})
+--add_includedirs("/usr/include", "/usr/local/include")
 --add_ldflags( "-ldl", "-latomic")
 add_requires("gtest")
 add_requires("gflags")
 
 add_requires("abseil")
-add_requires("grpc")
-add_requires("protobuf-cpp")
+add_requires("microsoft-proxy")
 --add_requires("prometheus")
 --
 --
@@ -46,42 +68,23 @@ add_requires("protobuf-cpp")
 --
 ----------------------------
 target("main")
-    add_packages("expat")
-    add_packages("civetweb")
-    add_packages("poco")
+    --add_packages("poco")
+    add_packages("matchit")
     add_packages("fmt")
     add_packages("spdlog")
-    add_packages("grpc")
-    add_packages("protobuf-cpp")
     add_packages("prometheus-cpp")
     add_packages("abseil")
+    add_packages("microsoft-proxy")
 
 -- cpp
     add_files("src/cmd/main.cpp|omain.cpp")
-    add_files("proto/*.cc")
-    add_files("proto/message.pb.grpc.cc")
 
 --
     add_includedirs("src")
-    add_includedirs("proto")
-
 -- lib include
 
 
 --link
---  add_rpathdirs("/usr/local/cuda-10.2/targets/x86_64-linux/lib/")
---  add_linkdirs("/usr/local/cuda-10.2/targets/x86_64-linux/lib/")
---  add_links("cudart")
---  add_linkdirs("/Users/meetai/.xmake/packages/g/grpc/v1.46.3/4890cb8637fd4f3ab06e1263224fdd77/lib")
-  add_linkdirs("/root/.xmake/packages/g/grpc/v1.46.3/8486e91ff6b74d5e8937b7a7afaa36c1/lib/")
-  add_links("protobuf")
-  add_links("protoc")
-  add_links("grpc++")
-  add_links("absl_time")
-  add_links("absl_city", "absl_raw_logging_internal", "absl_flags_config", "absl_scoped_set_env", "absl_flags_usage", "absl_strings_internal", "absl_failure_signal_handler", "absl_flags_parse", "absl_statusor", "absl_flags_marshalling", "absl_cord", "absl_exponential_biased", "grpc++", "protobuf-lite", "absl_examine_stack", "grpc++_unsecure", "absl_random_internal_distribution_test_util", "absl_bad_any_cast_impl", "gpr", "absl_leak_check", "absl_cordz_sample_token", "absl_cordz_functions", "absl_str_format_internal", "absl_random_internal_randen_slow", "grpc++_reflection", "absl_int128", "absl_cordz_info", "absl_flags_commandlineflag", "grpc++_alts", "absl_leak_check_disable", "absl_cord_internal", "absl_stacktrace", "absl_spinlock_wait", "absl_status", "absl_symbolize", "z", "grpc_unsecure", "absl_random_internal_platform", "absl_cordz_handle", "absl_flags_commandlineflag_internal", "absl_flags_program_name", "absl_random_seed_gen_exception", "absl_raw_hash_set", "absl_malloc_internal", "absl_bad_optional_access", "absl_flags_usage_internal", "re2", "absl_graphcycles_internal", "crypto", "absl_debugging_internal", "absl_base", "upb", "address_sorting", "absl_random_internal_randen", "absl_throw_delegate", "absl_strerror", "grpc", "absl_strings", "absl_flags_reflection", "absl_random_internal_randen_hwaes", "grpc_plugin_support", "absl_flags", "absl_flags_private_handle_accessor", "absl_random_internal_randen_hwaes_impl", "absl_random_internal_pool_urbg", "absl_flags_internal", "absl_random_distributions", "absl_time_zone", "absl_hashtablez_sampler", "absl_random_seed_sequences", "grpc++_error_details", "absl_low_level_hash", "absl_random_internal_seed_material", "protobuf", "protoc", "ssl", "cares", "absl_bad_variant_access", "absl_civil_time", "absl_synchronization", "absl_demangle_internal", "absl_periodic_sampler", "grpcpp_channelz", "absl_hash", "absl_log_severity", "absl_time")
-  add_links("gpr")
-  add_links("grpc")
-
 --  add_syslinks("pthread")
 
     set_kind("binary")
